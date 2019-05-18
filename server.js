332442b0a9689c6381f16d284ddf41095af868a3
Neto Chaves <netobac1@gmail.com>
2019-05-18 16:15:19 -0300

    firebase persistence and methods GET, POST and PUT

--- server.js
+++ server.js
@@ -1,20 +1,74 @@
 const express = require("express")
+const firebase = require("firebase")
+const bodyParser = require("body-parser")
+
 const app = express()
+
+app.use(bodyParser.json())
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBEoRVkLt1s_-KU_xDeuFc8qpCeK0hO3cI",
+  authDomain: "intelligym-7250e.firebaseapp.com",
+  databaseURL: "https://intelligym-7250e.firebaseio.com",
+  projectId: "intelligym-7250e",
+  storageBucket: "intelligym-7250e.appspot.com",
+  messagingSenderId: "427804816898",
+  appId: "1:427804816898:web:f277bb6ed4217362"
+}
+
+firebase.initializeApp(firebaseConfig)
 
 //to handle HTTP get request
 app.get("/", function(req, res) {
   console.log("Get Request")
-  res.send("Get Request")
+  const ref = firebase.database().ref("/Exercicios/")
+
+  ref.on(
+    "value",
+    function(snapshot) {
+      console.log(snapshot.val())
+      res.json(snapshot.val())
+      ref.off("value")
+    },
+    function(errorObject) {
+      console.log("The read failed: " + errorObject.code)
+      res.send("The read failed: " + errorObject.code)
+    }
+  )
 })
 
 app.post("/", function(req, res) {
   console.log("POST Request")
-  res.send("POST Request")
+
+  const nome = req.body.nome
+
+  const refPath = "/Exercicios/" + nome + "/"
+  const ref = firebase.database().ref(refPath)
+
+  ref.update({ nome }, function(error) {
+    if (error) {
+      res.send("Data could not be saved" + error)
+    } else {
+      res.send("Data saved successfully")
+    }
+  })
 })
 
 app.put("/", function(req, res) {
   console.log("PUT Request")
-  res.send("PUT Request")
+
+  const nome = req.body.nome
+
+  const refPath = "/Exercicios/" + nome + "/"
+  const ref = firebase.database().ref(refPath)
+
+  ref.set({ nome }, function(error) {
+    if (error) {
+      res.send("Data could not be saved" + error)
+    } else {
+      res.send("Data saved successfully")
+    }
+  })
 })
 
 app.delete("/", function(req, res) {
